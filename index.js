4484b40b3c262a1be33f066a2ec5ccc1bea97d4a
Melroy <rockstarodmel@gmail.com>
2022-12-15 12:20:29 +0300

    A much interaction with the console

--- index.js
+++ index.js
@@ -2,46 +2,34 @@ import readlineSync from "readline-sync";
 import Tesseract from "tesseract.js";
 import fs from "fs";
 import path from "path";
-import dotenv from "dotenv";
 import staffInfo from "./staffInfo.js";
-//loop throught all the files in the folder
-
-import axios from "axios";
 
 const workIt = async () => {
-  const currPath = "../Images";
-  const moveTo = "./Docs";
-  let AT;
-  dotenv.config();
+  const currPath = "../Scans";
+  // const moveTo = "./Docs";
+  const outPut = "../Output";
 
-  // Wait for user's response.
-  var fileDesc = readlineSync.question(
-    "What would you name your file? eg. StaffCode - <Resignation for ... / Continuation Letter>: "
+  const hrops = ["Continuation of work", "Acceptance of Resignation"];
+  let indexSel = readlineSync.keyInSelect(
+    hrops,
+    "Which type of Document you want to process"
   );
-  // console.log("Hi " + userName + "!");
 
-  // // Handle the secret text (e.g. password).
-  // var password = readlineSync.question("Please enter your password", {
-  //   hideEchoBack: true, // The typed text on screen is hidden by `*` (default).
-  // });
-  // console.log("Oh, " + userName + " loves " + favFood + "!");
+  let fileDesc;
 
-  //gen access-token
-  // await axios
-  //   .post(
-  //     `https://${process.env.APISERVER_REPOSITORY_API_BASE_URL}/v1/Repositories/${process.env.REPOSITORY_ID}/Token`,
-  //     {
-  //       grant_type: `password&username=${userName}&password=${password}`,
-  //     },
-  //     {
-  //       headers: {
-  //         "Content-Type": "application/x-www-form-urlencoded",
-  //       },
-  //     }
-  //   )
-  //   .then((response) => {
-  //     AT = response.data.access_token;
-  //   });
+  if (indexSel === 0) {
+    console.log(`You have selected: ${hrops[indexSel]}`);
+    fileDesc = readlineSync.question(
+      "What would you name your file? eg. StaffCode - <Continuation Letter>: "
+    );
+    console.log("Performing some Black magic");
+  } else {
+    console.log(`You have selected: ${hrops[indexSel]}`);
+    fileDesc = readlineSync.question(
+      "What would you name your file? eg. StaffCode - <Acceptance of Resignation >: "
+    );
+    console.log("Performing some magic that even i dont know");
+  }
 
   fs.readdir(currPath, function (err, files) {
     if (err) {
@@ -52,44 +40,40 @@ const workIt = async () => {
     files.forEach(function (file, index) {
       // Make one pass and make the file complete
       const fromPath = path.join(currPath, file);
-      const toPath = path.join(moveTo, file);
-      const fileName = path.basename(fromPath);
-
-      console.log("File Name", fileName);
+      // const toPath = path.join(moveTo, file);
+      // const fileName = path.basename(fromPath);
 
       Tesseract.recognize(fromPath, "eng").then(({ data: { text } }) => {
-        const staffName = text
-          .split("Dear")
-          .splice(1, 1)[0]
-          .toString()
-          .split("Thank")[0]
-          .trim()
-          .split("|")[0]
-          .trim();
+        let staffName;
 
-        console.log(staffName);
+        if (indexSel === 0) {
+          staffName = text
+            .split("Dear")
+            .splice(1, 1)[0]
+            .toString()
+            .split("The")[0]
+            .trim();
+        } else if (indexSel === 1) {
+          staffName = text
+            .split("Dear")
+            .splice(1, 1)[0]
+            .toString()
+            .split("Thank")[0]
+            .trim()
+            .split("|")[0]
+            .trim();
+        }
+        console.log("Name:", staffName);
         //get the staffCode
         for (var i = 0; i < staffInfo.length; i++) {
           if (staffInfo[i].preff_name === staffName) {
             const staffCode = staffInfo[i].staff_code;
-            console.log(staffCode);
-            //checking if the staffCode Folder Exists
-            const empPath = `./Docs`;
-            // const isFolderExist = fs.existsSync(empPath); //something staffcode
-            // if (isFolderExist === false) {
-            //   fs.mkdirSync(empPath);
-            // }
-            const newFileName = `${empPath}/${staffCode} - ${fileDesc}.jpg`;
-            //save the file in that folder
-
-            //upload to LF
-            // await axios.post("https://api.laserfiche.com/repository/v1/Repositories/repoId/Entries/parentfolderid/${}")
-
+            const newFileName = `${outPut}/${staffCode} - ${fileDesc}.jpg`;
             fs.copyFile(fromPath, newFileName, (err) => {
               if (err) {
                 console.error("Error", err);
               }
-              console.log(`Done for ${staffCode} `);
+              console.log(`Done for ${staffCode}`);
             });
           }
         }
